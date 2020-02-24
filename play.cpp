9cec8ef2685b9e2674347a84b37aaabbc3007dad
apple-chip <61082478+apple-chip@users.noreply.github.com>
2020-02-24 11:15:18 +0800

    编码图像生成演示

--- play.cpp
+++ play.cpp
@@ -1,43 +1,86 @@
 #include <fstream>
+#include <vector>
 #include <opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
-
-int main()
+class Code4D
 {
-	int code_size = 80;
-	Mat mat(code_size + 14, code_size + 14, CV_8UC3, Scalar(255, 255, 255));
-	for (int i = 0; i < 7; ++i)
-		for (int j = 0; j < 7; ++j)
-			mat.at<Vec3b>(i, j) = mat.at<Vec3b>(i, code_size + 13 - j) = mat.at<Vec3b>(code_size + 13 - i, j) = Vec3b(0, 0, 0);
-	for (int i = 1; i < 6; ++i)
-		for (int j = 1; j < 6; ++j)
-			mat.at<Vec3b>(i, j) = mat.at<Vec3b>(i, code_size + 13 - j) = mat.at<Vec3b>(code_size + 13 - i, j) = Vec3b(255, 255, 255);
-	for (int i = 2; i < 5; ++i)
-		for (int j = 2; j < 5; ++j)
-			mat.at<Vec3b>(i, j) = mat.at<Vec3b>(i, code_size + 13 - j) = mat.at<Vec3b>(code_size + 13 - i, j) = Vec3b(0, 0, 0);
+	int rows, cols, channels;
+	Mat _t;
+	unsigned bits;
+	vector<Mat> frame;
 
-	ifstream fin("file", ios::binary);
-	int size = 0;
+public:
+	Code4D(int _rows, int _cols) : _t(_rows, _cols, CV_8UC3, Scalar(255, 255, 255))
+	{
+		bits = 0;
+		cols = _cols;
+		rows = _rows;
+		for (int i = 0; i < 7; ++i)
+			for (int j = 0; j < 7; ++j)
+				_t.at<Vec3b>(i, j) = _t.at<Vec3b>(i, cols - 7 + j) = _t.at<Vec3b>(rows - 7 + i, j) = Vec3b(0, 0, 0);
+		for (int i = 1; i < 6; ++i)
+			for (int j = 1; j < 6; ++j)
+				_t.at<Vec3b>(i, j) = _t.at<Vec3b>(i, cols - 7 + j) = _t.at<Vec3b>(rows - 7 + i, j) = Vec3b(255, 255, 255);
+		for (int i = 2; i < 5; ++i)
+			for (int j = 2; j < 5; ++j)
+				_t.at<Vec3b>(i, j) = _t.at<Vec3b>(i, cols - 7 + j) = _t.at<Vec3b>(rows - 7 + i, j) = Vec3b(0, 0, 0);
+		frame.push_back(_t.clone());
+	}
+	void pushByte(unsigned char c)
+	{
+		for (int i = 0; i < 8; ++i)
+		{
+			bool is = (c >> i) & 0x01;
+
+			int _rgb = bits % 3;
+			unsigned _frame = bits / ((cols * rows - 3 * 8 * 8) * 3);
+			if (_frame + 1 > frame.size())
+				frame.push_back(_t.clone());
+
+			unsigned zone_a = (cols - 2 * 8) * 8 * 3;
+			unsigned zone_b = (rows - 2 * 8) * cols * 3;
+
+			unsigned _c, _r;
+			unsigned bit_in_frame = bits % ((cols * rows - 3 * 8 * 8) * 3);
+			if (bit_in_frame < zone_a)
+				_c = (bit_in_frame / 3) % (cols - 2 * 8) + 8,
+				_r = (bit_in_frame / 3) / (cols - 2 * 8);
+			else if (bit_in_frame < zone_a + zone_b)
+				_c = ((bit_in_frame - zone_a) / 3) % cols,
+				_r = ((bit_in_frame - zone_a) / 3) / cols + 8;
+			else
+				_c = ((bit_in_frame - zone_a - zone_b) / 3) % (cols - 8) + 8,
+				_r = ((bit_in_frame - zone_a - zone_b) / 3) / (cols - 8) + rows - 8;
+			frame.back().at<Vec3b>(_r, _c).val[_rgb] = is * 255;
+			bits++;
+		}
+	}
+	void save(const string &dir)
+	{
+		for (size_t i = 0; i < frame.size(); i++)
+		{
+			char buf[20];
+			sprintf(buf, "img%03ld.png", i);
+			imwrite(dir + string(buf), frame[i]);
+		}
+	}
+};
+
+int main(int argc, char **argv)
+{
+	int cols, rows;
+	sscanf(argv[3], "%d", &cols);
+	sscanf(argv[4], "%d", &rows);
+	Code4D code(rows, cols);
+	ifstream fin(argv[1], ios::binary);
 	while (fin.good())
 	{
 		char byte;
 		fin.read(&byte, 1);
-		for (int i = 0; i < 8; ++i)
-		{
-			bool is = (byte >> i) & 0x01;
-			int all = 8 * size + i;
-			int z = all % 3;
-			int y = ((all - z) / 3) % code_size;
-			int x = all / (3 * code_size);
-			mat.at<Vec3b>(x + 7, y + 7).val[z] = is * 255;
-			printf("bit %d: [%d,%d]%d %d\n", all, x, y, z, is);
-		}
-		size++;
+		code.pushByte(byte);
 	}
-
-	imshow("img", mat);
+	code.save(argv[2]);
 	waitKey(0);
-	imwrite("img.png", mat);
 	return 0;
 }
